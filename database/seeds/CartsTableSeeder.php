6114692b8f205cb39923d134877e58c08d6949ac
tktced <tktced@gmail.com>
2018-01-13 14:26:13 -1000

    CartsTableSeeder and Model

--- database/seeds/CartsTableSeeder.php
+++ database/seeds/CartsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Cart;
 
 class CartsTableSeeder extends Seeder
 {
@@ -11,6 +13,27 @@ class CartsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $carts = [];
+
+        for($i = 0; $i < 10; $i++):
+            $userId = rand(1, 10);
+            $total = rand(0, 100000);
+            $userPaymentMode = rand(1, 6);
+            $shippingAddress_id = rand(1, 15);
+            $billingAddress_id = rand(1, 15);
+
+            $carts[] = [   
+                'user_id' => $userID,
+                'total' => $total,
+                'UserPaymentMode_id' => $userPaymentMode,
+                'shippingAddress_id' => $shippingAddress_id,
+                'billingAddress_id' => $billingAddress_id,
+            ];
+        endfor;
+
+        foreach($carts AS $cart):
+
+            Message::create($cart);
+        endforeach;
     }
 }
