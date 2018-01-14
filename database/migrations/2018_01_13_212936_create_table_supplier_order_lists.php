a3e5ce166c6a515c9bc7273a73b1b4a6c2bd962f
gaby <chansaud.eremoana@gmail.com>
2018-01-13 14:10:41 -1000

    ajout des parametres de la table supplier_order_lists dans migration

--- database/migrations/2018_01_13_212936_create_table_supplier_order_lists.php
+++ database/migrations/2018_01_13_212936_create_table_supplier_order_lists.php
@@ -14,6 +14,18 @@ class CreateTableSupplierOrderLists extends Migration
     public function up()
     {
         //
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('suppplier_id');
+            $table->integer('supplier_order_id');
+            $table->integer('produit_id');
+            $table->integer('qty');
+            $table->date('date');
+            $table->time('time');
+            
+            $table->timestamps();
+        });
+        
     }
 
     /**
