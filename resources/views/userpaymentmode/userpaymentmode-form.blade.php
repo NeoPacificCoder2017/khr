75c4945b5fc3297512aa4d45e4aaf49ca0a766f7
Parea <taie.taataparea1@gmail.com>
2018-01-13 16:04:47 -1000

    modified

--- resources/views/userpaymentmode/userpaymentmode-form.blade.php
+++ resources/views/userpaymentmode/userpaymentmode-form.blade.php
@@ -1,5 +1,5 @@
-<form action="http://khr.taataparea.npc/userpaymentmodes" method="POST">
+{!! Form::open(['url' => 'userpaymentmodes']) !!}
   User Id : <input type='text' name='user_id' placeholder='Utilisateur'/><br/>
   Payment_mode_id : <input type='text' name='payment_mode_id' placeholder='Paiement'/>
   <input type='submit' name='valider'/>
-</form>
+{!! Form::close() !!}
