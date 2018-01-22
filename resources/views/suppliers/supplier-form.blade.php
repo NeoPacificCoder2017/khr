63ec4bc9fb8660557eb07155283076992b2b90be
gaby <chansaud.eremoana@gmail.com>
2018-01-22 11:58:18 -1000

    ajout des formulaires de la vue formulaire

---
+++ resources/views/suppliers/supplier-form.blade.php
@@ -0,0 +1,11 @@
+{!! Form::open(['url' => 'supplier']) !!}
+
+    supplier_Id : <input type='text' name='supplier_Id'/>
+
+    name : <input type='text' name='name'/>
+    last_name : <input type='text' name='nalast_nameme'/>
+    first_name : <input type='text' name='first_name'/>
+    email : <input type='text' name='email'/>
+    celphone : <input type='text' name='celphone'/>
+    <input type="submit" value="valider"/>
+{!! Form::close() !!}
