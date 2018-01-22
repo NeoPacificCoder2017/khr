097a6786992cb06ac60c57494bf9e4f414b260f3
gaby <chansaud.eremoana@gmail.com>
2018-01-22 10:32:14 -1000

    modification des fonctions du SupplierController

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,9 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\suppliers;
 
 class SupplierController extends Controller
 {
-    //
+    
+    public function all() {
+        $supplier = suppliers::all();
+
+        dump($supplier);
+        return view('suppliers.suppliers');
+    }
+
+    public function show() {
+        $supplier = suppliers::find($supplierId);
+        dump($supplier);
+        return view('suppliers.supplier');
+    }
+
+    public function new() {
+        return view('suppliers.supplier-form');
+    }
+
+    public function create() {
+        $input = $request->all();
+
+        $supplier = new Suppliers();
+        $supplier ->suppler_id = $input['supplier_id'];
+        $supplier ->name = $input['name'];
+        $supplier ->last_name = $inpute['last_name'];
+        $supplier ->first_name = $inpute['first_name'];
+        $supplier ->celphone = $inpute['celphone'];
+        $message->save();
+
+    }
+
+    
 }
