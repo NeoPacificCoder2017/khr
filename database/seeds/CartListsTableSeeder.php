37261a78d93147666551e181560be6d82e8ec4b3
tktced <tktced@gmail.com>
2018-01-22 09:36:57 -1000

    seeder table

---
+++ database/seeds/CartListsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\CartList;
+
+class MessagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $cartLists = [];
+
+        for($i = 0; $i < 10; $i++):
+            $userID = rand(1, 10);
+            $validated = rand(0, 2);
+            $validatedBy = ($validated == 0)?0:1;
+
+            $messages[] = [   
+                'user_id' => $userID,
+                'message' => str_random(10),
+                'validated' => $validated,
+                'validated_by' => $validatedBy,
+            ];
+        endfor;
+
+        foreach($messages AS $message):
+
+            Message::create($message);
+        endforeach;
+    }
+}
