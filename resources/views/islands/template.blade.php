73836d3deda4c956c74542b42598667cf6e6e1aa
Sheinehah <maui.sheinehah@gmail.com>
2018-01-22 14:46:08 -1000

    Création d'un template

---
+++ resources/views/islands/template.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/style">
+        <title>KAI HOTU RAU </title>
+        {!! Html::style('https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css') !!}
+        {!! Html::style('https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css') !!}
+     </head>
+     <style> textarea { resize: none; } </style>
+    </head>
+    <body>
+        @yield('contenu')
+    </body>
+</html>
