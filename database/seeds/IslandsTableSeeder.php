44aeb8c9f8f407f0e40b00895dcf6961604c9da2
Sheinehah <maui.sheinehah@gmail.com>
2018-01-22 14:02:35 -1000

    Modification du seed, et du call (databaseSeeder)

--- database/seeds/IslandsTableSeeder.php
+++ database/seeds/IslandsTableSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Island;
+
 
 class IslandsTableSeeder extends Seeder
 {
@@ -13,10 +15,16 @@ class IslandsTableSeeder extends Seeder
      */
     public function run()
     {
-        $name_islands = array('name' => array('Tahiti', 'Bora Bora', 'Moorea', 'Huahine', 'Raiatea', 'Tahaa', 'Maupiti', 
+        $name_islands = ['Tahiti', 'Bora Bora', 'Moorea', 'Huahine', 'Raiatea', 'Tahaa', 'Maupiti', 
         'Rangiroa', 'Manihi', 'Tikehau', 'Fakarava', 'Ahe', 'Mangareva', 'Nuku Hiva', 'Hiva Oa', 'Ua Pou', 
-        'Rurutu', 'Tubuai', 'Raivavae', 'Rimatara', 'Makemo' )
-                            );
+        'Rurutu', 'Tubuai', 'Raivavae', 'Rimatara', 'Makemo' ];
+        //DB::table('islands')->delete();                   
+
+        foreach($name_islands AS $island):
+            Island::create(array('name' => $island));
+        endforeach;
+
+                            
     
     }
 }
