08575f37ae6afe49bb426bdacf16154153aa7d0b
vaiariiTepa <tepavaiarii@gmail.com>
2018-01-22 15:00:03 -1000

    tableau all()

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,1 +1,36 @@
 
+</br>
+Action :
+<select>
+    <option>Delete</option>
+    <option>Update</option>
+</select>
+
+</br></br>
+
+<table>
+    <tr>
+        <th></th>
+        <th>id</th>
+        <th>title</th>
+        <th>prop2</th>
+        <th calspan="2">Actions</th>
+    </tr>
+    <tr>
+        
+        @foreach(App\categories::all() as $categorie)
+            <tr>
+            <td><input type="checkbox"/></td>
+            <td>{{$categorie -> id}}</td>
+            <td>{{$categorie -> nom}}</td>
+            <td>mon prop2</td>
+            <td><button>Delete</button></td>
+            <td><button>Update</button></td>
+            </tr>
+        @endforeach    
+
+       
+
+    </tr>
+</table>
+</html>
